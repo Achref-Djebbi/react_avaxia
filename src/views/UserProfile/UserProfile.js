416522e332d36f4a7b7175c8d4c3cf3632c34d59
akkariaymen <akkariaymen@outlook.com>
2020-10-07 16:40:03 -0700

    update monitorig page add select landscape and systems

--- src/views/UserProfile/UserProfile.js
+++ src/views/UserProfile/UserProfile.js
@@ -13,6 +13,18 @@ import TextField from '@material-ui/core/TextField';
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
 
+//import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+//import { Landscape } from "@material-ui/icons";
+
 
 
 const styles = {
@@ -31,16 +43,112 @@ const styles = {
     fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
     marginBottom: "3px",
     textDecoration: "none"
-  }
+  },
+
+  formControl: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+    minWidth: 120,
+  },
+
 };
 
 const useStyles = makeStyles(styles);
 
 export default function UserProfile() {
   const classes = useStyles();
+  
+  const [open, setOpen] = React.useState(false);
+  const [ccbji, setc] = React.useState('');
+  const [quadient, setq] = React.useState('');
+  const [system, setl] = React.useState('');
+  const [landscape, sets] = React.useState('');
+
+  const handleChange = (event) => {
+    setc(Number(event.target.value) || '');
+    setq(Number(event.target.value) || '');
+    setl(Number(event.target.value) || '');
+    sets(Number(event.target.value) || '');
+  };
+
+  
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <div>
       <GridContainer>
+
+
+
+
+        
+      <GridItem xs={12} sm={12} md={12}>
+          <Card>
+                <CardBody>
+                <Button onClick={handleClickOpen}>Select The LandScape and the Systems</Button>
+      <Dialog disableBackdropClick disableEscapeKeyDown open={open} onClose={handleClose}>
+        <DialogTitle>Fill the form</DialogTitle>
+        <DialogContent>
+          <form className={classes.container}>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="demo-dialog-native">LandScape</InputLabel>
+              <Select
+                native
+                value={landscape}
+                onChange={handleChange}
+                input={<Input id="demo-dialog-native" />}
+              >
+                <option aria-label="None" value="" />
+                <option>CCBJI</option>
+                <option>Quadient</option>
+                
+              </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <InputLabel id="demo-dialog-select-label">System</InputLabel>
+              <Select
+                labelId="demo-dialog-select-label"
+                id="demo-dialog-select"
+                value={system}
+                onChange={handleChange}
+                input={<Input />}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem >PJR</MenuItem>
+                <MenuItem >PJE</MenuItem>
+                <MenuItem >PJA</MenuItem>
+              </Select>
+            </FormControl>
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
+                </CardBody>
+          </Card>
+      </GridItem>
+
+
+
+
         <GridItem xs={12} sm={12} md={12}>
           <Card>
             <CardHeader color="primary">
